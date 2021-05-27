66052029324301a0987bd7f1576d159692db6ef6
ozlia <63154309+ozlia@users.noreply.github.com>
2021-05-27 18:45:30 +0300

    filter and sort

--- project/routes/search.js
+++ project/routes/search.js
@@ -2,11 +2,24 @@ var express = require("express");
 var router = express.Router();
 const players_utils = require("./utils/players_utils");
 const teams_utils = require("./utils/team_utils");
-
+const search_utils = require("./utils/search_utils");
+     
 
 router.get("/player/search", async (req, res, next) => {
     try {
       const results = await players_utils.getPlayersByName(req.query.player_name);
+      if (req.query.filtered_by && req.query.filtered_by == "position"){
+        results = search_utils.filterArray(results, "position", req.query.filter);
+      }
+      if (req.query.filtered_by && req.query.filtered_by == "team name"){
+        results = search_utils.filterArray(results, "team_name", req.query.filter);
+      }
+      if (req.query.sorted && req.query.sorted == "team name"){
+        search_utils.sortArray(results, "team_name");
+      }
+      if (req.query.sorted && req.query.sorted == "player name"){
+        search_utils.sortArray(results, "name");
+      }
       res.status(200).send(results);
     } catch (error) {
       next(error);
@@ -16,6 +29,12 @@ router.get("/player/search", async (req, res, next) => {
 router.get("/team/search", async (req, res, next) => {
     try {
         const results = await teams_utils.getTeamsByName(req.query.team_name);
+        console.log(results)
+        if (req.query.sorted){
+            search_utils.sortArray(results, function(a,b){
+                return a.fullname - b.fullname;
+            })
+        }
         res.status(200).send(results);
     } catch (error) {
         next(error);
--- project/routes/utils/serach_utils.js
+++
@@ -1,4 +0,0 @@
-const axios = require("axios");
-const api_domain = "https://soccer.sportmonks.com/api/v2.0";
-// const player_name = "James";
-
