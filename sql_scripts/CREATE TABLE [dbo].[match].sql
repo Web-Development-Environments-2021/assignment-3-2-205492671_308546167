7cf685b8009a217013a708dcf40a49d44acabb3c
Naor <Naors1144@gmail.com>
2021-05-27 14:05:21 +0300

    API updated

--- sql_scripts/CREATE TABLE [dbo].[match].sql
+++ sql_scripts/CREATE TABLE [dbo].[match].sql
@@ -2,6 +2,8 @@ CREATE TABLE [dbo].[match](
 	match_id [varchar] IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	home_team [varchar](300) NOT NULL UNIQUE,
 	away_team [varchar](300) NOT NULL,
+	court [varchar] (300) NOT NULL,
 	referee_team [varchar](300) NOT NULL,
 	date [varchar](300) NOT NULL,
+	score [varchar] (300) NOT NULL
 )
