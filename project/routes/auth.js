32a1695d50f7e29ea9e3f33eb7ac920f81194163
ozlia <63154309+ozlia@users.noreply.github.com>
2021-06-01 21:15:17 +0300

    loggedout

--- project/routes/auth.js
+++ project/routes/auth.js
@@ -64,8 +64,11 @@ router.post("/login", async (req, res, next) => {
 });
 
 router.post("/Logout", function (req, res) {
+  if (!req.session || !req.session.user_id){
+    throw { status: 412, message: "no user is logged in" };
+  }
   req.session.reset(); // reset the session info --> send cookie when  req.session == undefined!!
-  res.send({ success: true, message: "logout succeeded" });
+  res.status(200).send("logout succeeded");
 });
 
 module.exports = router;
