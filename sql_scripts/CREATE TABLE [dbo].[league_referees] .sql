b8abd1dd60789359dc48036d1f206fc38260f2ff
Naor <Naors1144@gmail.com>
2021-06-04 16:59:15 +0300

    league_referees db sql script fixed

--- sql_scripts/CREATE TABLE [dbo].[league_referees] .sql
+++ sql_scripts/CREATE TABLE [dbo].[league_referees] .sql
@@ -1,5 +1,7 @@
 CREATE TABLE [dbo].[league_referees](
 	user_id [int] NOT NULL,
 	league_id [varchar](300) NOT NULL,
-	PRIMARY KEY(user_id, league_id)
+	PRIMARY KEY(user_id, league_id),
+	CONSTRAINT FK_league_ref FOREIGN KEY (user_id)
+	REFERENCES users(user_id)
 )
